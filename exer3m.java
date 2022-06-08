3f9c1373fe4a61dd30cc5e037382795df696e815
Victor Casagrande Correa <107128755+CasagrandeVictor@users.noreply.github.com>
2022-06-08 15:13:43 -0300

    Add files via upload

---
+++ exer3m.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class exer3m {
+    public static void main(String[] args) {
+        Scanner  input = new Scanner(System.in);
+        Integer a,b,c,d;
+        a = (3-2-1+2+1+3);
+        b = (2*3)-(4*5);
+        c = (2+6-3)/(7*9);
+        d = (3 % 4) - 8;
+        System.out.println("Resultado do A: "+ a );
+        System.out.println("Resultado do B: "+ b ); 
+        System.out.println("Resultado do C: "+ c ); 
+        System.out.println("Resultado do D: "+ d ); 
+    } 
+}
