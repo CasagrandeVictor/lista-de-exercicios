f4105c41b726e0ee9948ab55ef68593f4fbcf6ba
Victor Casagrande Correa <107128755+CasagrandeVictor@users.noreply.github.com>
2022-06-08 15:10:21 -0300

    Add files via upload

---
+++ exer1m.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class exer1m{
+    public static void main(String[] args) {
+    Scanner  input = new Scanner(System.in);
+    Double h,b,B,a;
+    System.out.println("Digite o valor da altura: ");
+    h = input.nextDouble();
+    System.out.println("Digite o valor da base menor: ");
+    b = input.nextDouble();
+    System.out.println("Digite o valor da base maior: ");
+    B = input.nextDouble();
+
+    a = (h*(b + B))/2;
+
+    System.out.println("O valor da área do trapézio: "+ a +"cm");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
