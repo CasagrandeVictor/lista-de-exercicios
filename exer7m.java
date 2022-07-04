81b3a53633363bf627ebd6d8ebb4ff9054f4a580
Victor Casagrande Correa <107128755+CasagrandeVictor@users.noreply.github.com>
2022-07-04 08:48:11 -0300

    Add files via upload

---
+++ exer7m.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exer7m {
+    public static void main(String[] args) {
+        Scanner  input = new Scanner(System.in);
+    double total,valorhora,hora;
+
+    valorhora = 10.25;
+    System.out.println("Digite as horas trabalhadas: ");
+    hora = input.nextDouble();
+    total = hora * valorhora;
+    System.out.println("Seu Salário de acordo com as horas :R$"+total);
+
+
+
+
+
+    
+    
+    }
+
+
+
+
+
+}
